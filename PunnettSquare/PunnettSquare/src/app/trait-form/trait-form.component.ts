5c5ce51f9916f74fc12c9ee6d88d644595151f06
JakobLopez <thejakoblopez@gmail.com>
2019-11-09 17:56:50 -0600

    radio buttons get ssaved in array

--- PunnettSquare/PunnettSquare/src/app/trait-form/trait-form.component.ts
+++ PunnettSquare/PunnettSquare/src/app/trait-form/trait-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-trait-form',
@@ -10,10 +11,37 @@ export class TraitFormComponent implements OnInit {
   numberOfTraits: Number = 0;
   parent1Traits = [];
   aprent2Traits = [];
+  form;
+  parent1 = {
+    traits: [
+      { name: 'homozygous-d',  selected: true, id: 1 },
+      { name: 'heterozygous',  selected: false, id: 2 },
+      { name: 'homozygous-r',  selected: false, id: 3 },
+    ]
+  }
 
-  constructor() { 
+  constructor(private formBuilder: FormBuilder) { 
+    this.form = this.formBuilder.group({
+      traits: this.buildTraits()
+    });
   }
 
   ngOnInit() {
+  }
+
+  buildTraits() {
+    const arr = this.parent1.traits.map(trait => {
+      return this.formBuilder.control(trait.selected);
+    });
+    return this.formBuilder.array(arr);
+  }
+
+  get traits() {
+    return this.form.get('traits');
+  };
+
+  display(){
+    console.log("eyer");
+    console.log(this.parent1Traits);
   }
 }
